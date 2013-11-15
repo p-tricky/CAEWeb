acac638497878c92561c5e7ef4d28a3bbeae47b7
DJBarnes <david.j.barnes@wmich.edu>
2013-11-15 16:04:16 -0500

    Add update functionality. Still need to finish delete.

--- app/controllers/roomscheduleController.php
+++ app/controllers/roomscheduleController.php
@@ -1,5 +1,7 @@
 <?php
-  
+ 
+use Illuminate\Database\Eloquent\Collection as BaseCollection;
+
 class RoomScheduleController extends BaseController {
   
   public function getIndex()
@@ -11,31 +13,48 @@ class RoomScheduleController extends BaseController {
 
   public function postNew()
   {
-    $theModels = Input::get("models");
-    foreach ($theModels as $model) {
-      //if ($model['id'] == 0) {
-        $newClassroom = new Classroom;
-        $newClassroom->RoomId = $model['RoomId'];
-        $newClassroom->Title = $model['Title'];
-        $newClassroom->Start = date('Y-m-d H:i:s',(strtotime($model['Start'])-18000));
-        $newClassroom->End = date('Y-m-d H:i:s',(strtotime($model['End'])-18000));
-        $newClassroom->Attendee = $model['Attendee'];
-        $newClassroom->RecurrenceId = $model['RecurrenceId'];
-        $newClassroom->RecurrenceRule = $model['RecurrenceRule'];
-        $newClassroom->RecurrenceException = $model['RecurrenceException'];
+    $NewModels = Input::get("models");
+    foreach ($NewModels as $model) {
 
-        $newClassroom->save();
+      $newClassroom = new Classroom;
+      $newClassroom->RoomId = $model['RoomId'];
+      $newClassroom->Title = $model['Title'];
+      $newClassroom->Start = date('Y-m-d H:i:s',(strtotime($model['Start'])-18000));
+      $newClassroom->End = date('Y-m-d H:i:s',(strtotime($model['End'])-18000));
+      $newClassroom->Attendee = $model['Attendee'];
+      $newClassroom->RecurrenceId = $model['RecurrenceId'];
+      $newClassroom->RecurrenceRule = $model['RecurrenceRule'];
+      $newClassroom->RecurrenceException = $model['RecurrenceException'];
 
-        return $newClassroom->toJson();
-      //}
+      $newClassroom->save();
+
+      return $newClassroom->toJson();
+
     }
   }
 
   public function putUpdate()
   {
-    $theModel = Input::get("models");
-    $modelId = $theModel[0]['id'];
-    return $modelId;
+    $modelArray = array();
+    $updateModels = Input::get("models");
+    foreach ($updateModels as $model) {
+      
+      $updateClassroom = Classroom::find($model['id']);
+      $updateClassroom->RoomId = $model['RoomId'];
+      $updateClassroom->Title = $model['Title'];
+      $updateClassroom->Start = date('Y-m-d H:i:s',(strtotime($model['Start'])-18000));
+      $updateClassroom->End = date('Y-m-d H:i:s',(strtotime($model['End'])-18000));
+      $updateClassroom->Attendee = $model['Attendee'];
+      $updateClassroom->RecurrenceId = $model['RecurrenceId'];
+      $updateClassroom->RecurrenceRule = $model['RecurrenceRule'];
+      $updateClassroom->RecurrenceException = $model['RecurrenceException'];
+
+      $updateClassroom->save();
+
+      array_push($modelArray, $updateClassroom);
+      $returnModels = BaseCollection::make($modelArray);
+    }
+    echo $returnModels->toJson();
   }
 
   public function deleteClassroom()
