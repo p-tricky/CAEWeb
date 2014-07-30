4a59361e73a5781aeb710c6fe64e38ce47919277
DJBarnes <david.j.barnes@wmich.edu>
2014-07-30 15:22:24 -0400

    Add comments to the room scheduler javascript section.

--- public/js/roomschedule/controllers/breakoutRoomController.js
+++ public/js/roomschedule/controllers/breakoutRoomController.js
@@ -1,19 +1,30 @@
+//Define a Breakout Room Tab module for the kendo scheduler to line in.
 RoomScheduleApp.module('RoomScheduleBreakoutRoomTab', function (RoomScheduleBreakoutRoomTab, App, Backbone, Marionette, $, _) {
+
+  //Define a breakout room controller for all the functions related to special rooms
   RoomScheduleBreakoutRoomTab.BreakoutRoomController = {
 
+    //Function to display the scheduler for the breakout rooms
     showRoomSchedule : function() {
+      //Create a TODAY date
       var TODAY = new Date();
+      //Use jQuery to select the div for the scheduler to live in, and call the scheduler method
       $("#tabsContent").kendoScheduler({
           date: new Date(),
+          //Limit the start and end time to 8:00am and 10:00pm.
           startTime: new Date(TODAY.getFullYear(), TODAY.getMonth(), TODAY.getDate(), 8, 0, 0),
           endTime: new Date(TODAY.getFullYear(), TODAY.getMonth(), TODAY.getDate(), 22, 0, 0),
+          //Define a height that fits the entire set of time slots in.
           height: 970,
           allDaySlot: false,
 
+          //List the views that can be seen. Day is the only one we will use.
           views: [
               "day"
           ],
           timezone: "Etc/UTC",
+          //Setup the datasource for the rooms. The URL property will be used to persist the data.
+          //ajax requests to that url will be picked up by the laravel router, and then executed.
           dataSource: {
             batch: true,
               transport: {
@@ -36,10 +47,12 @@ RoomScheduleApp.module('RoomScheduleBreakoutRoomTab', function (RoomScheduleBrea
                   type:'DELETE',
                   dataType: "json"
                 },
+                //I can't remember why this parameter map is here, but it is needed.
                 parameterMap: function(data, type) {
                   return kendo.stringify(data);
                 }
               },
+              //This section describes what the model for the data will look like.
               schema: {
                   model: {
                       id: "Id",
@@ -59,11 +72,15 @@ RoomScheduleApp.module('RoomScheduleBreakoutRoomTab', function (RoomScheduleBrea
                   }
               }
           },
+          //Define how to group the resources
           group: {
               resources: [ "room" ]
           },
+          //Additional resources for the scheduled events.
           resources: [
               {
+                  //Attendee determines if it is a class or event. It should have a more meaningful name
+                  //other than attendee, but that was the kendo default.
                   field: "attendee",
                   dataSource: [
                       { text: "Class", value: 1, color: "#eeb111" },
@@ -72,6 +89,10 @@ RoomScheduleApp.module('RoomScheduleBreakoutRoomTab', function (RoomScheduleBrea
                   title: "Type"
               },
               {
+                  //This is the rooms list for each schedule. This could be setup with a datasource so that it could
+                  //be pulled from a database, but for now, it is hard coded in. The employee schedule has resources
+                  //being pulled from a database. Refer to how it is done in the employee tab if you want to change
+                  //how this list of classes is set.
                   field: "roomId",
                   name: "room",
                   dataSource: [
