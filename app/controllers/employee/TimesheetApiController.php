f5f6f6ce7b2a7aa0a0856ab2684d713d558fe3dc
Dan Peekstok <daniel.g.peekstok@wmich.edu>
2015-06-08 11:35:12 -0400

    Fixed Timesheet sorting and hours are calulated in decimal instead of H:M

--- app/controllers/employee/TimesheetApiController.php
+++ app/controllers/employee/TimesheetApiController.php
@@ -47,11 +47,45 @@ class TimesheetApiController extends BaseController
         $end = date('Y-m-d', strtotime($end . ' + 1 day'));
 
         //get shifts in specified range
-        $shifts = Shift::where('clockOut', '<=', $end)->where('clockIn', '>=', $start)->orderBy('eid')->orderBy('clockIn')->get();
+        $shifts = Shift::where('clockOut', '<=', $end)->where('clockIn', '>=', $start)->get();
+
+        //adds the user's name to each shift
+        foreach ($shifts as $shift) {
+            $shift->name = User::where('id', '=', $shift->eid)->pluck('fullname');
+        }
+
+        //sorts the shifts first by user's last name then by clockin
+        $shifts = $shifts->sortBy(function($shift){
+            //gets the number of names-1. E.g. if the user has first, middle, and last names, it will be 2
+            //if the user has two last names it will be 3
+            $names = substr_count($shift->name, ' ');
+            switch ($names) {
+                case 1:
+                    //pos is the position of the space before the last name
+                    $pos = strpos($shift->name, ' ');
+                    break;
+                case 2:
+                case 3:
+                    //has to offset by the index of the first space +1
+                    $pos = strpos($shift->name, ' ', strpos($shift->name, ' ')+1);
+                default:
+                    //if someone has more than names, it will just sort by the second name 
+                    $pos = strpos($shift->name, ' ');
+                    break;
+            }
+            //this will return the last name concatinated with the clockin. This is the string that its sorted by
+            return substr($shift->name , $pos) . $shift->clockIn;
+        });
+
+        //removes the index values from each item. If left, Backbone wont recognize it properly
+        $shifts->values();
+
+        //returns the shifts
         return $shifts->toJSON();
 	}
 
     //returns the user name based on an id
+    //currently unused but will be left in for future use
 	public function getUserName() {
         //gets the id from the input
 		$userId = Input::get('id');
