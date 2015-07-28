4094c09cd7e3bfe374c386059a09fe6e1086e2ec
Dan Peekstok <daniel.g.peekstok@wmich.edu>
2015-07-28 15:07:30 -0400

    Added sortby method to users in the Virus Users tab

--- app/controllers/systemadmin/VirusUserApiControllor.php
+++ app/controllers/systemadmin/VirusUserApiControllor.php
@@ -3,7 +3,29 @@ class VirusUserApiController extends BaseController {
 
   public function index() {
     try {
-      $scanUsers = ScansUser::orderBy('total', 'DESC')->get();
+      $sortBy = Input::get('sort', 'totalDesc');
+      switch ($sortBy)
+      {
+        case "nameAsc":
+          $scanUsers = ScansUser::orderBy('user_name')->get();
+          break;
+        case "nameDesc":
+          $scanUsers = ScansUser::orderBy('user_name', 'DESC')->get();
+          break;
+        case "totalAsc":
+          $scanUsers = ScansUser::orderBy('total')->get();
+          break;
+        case "totalDesc":
+          $scanUsers = ScansUser::orderBy('total', 'DESC')->get();
+          break;
+        case "dateAsc":
+          $scanUsers = ScansUser::orderBy('last_scanned')->get();
+          break;
+        case "dateDesc":
+          $scanUsers = ScansUser::orderBy('last_scanned', 'DESC')->get();
+          break;
+      }
+      
       return $scanUsers->toJSON();
     } catch(Exception $e) {
       return json_encode('{"error":{"text":' . $e->getMessage() . '}}');
--- public/js/systemadmin/views/virusUsers/virusUserCompositeView.js
+++ public/js/systemadmin/views/virusUsers/virusUserCompositeView.js
@@ -12,6 +12,15 @@ SysAdminApp.module('VirusUserTab', function (VirusUserTab, App, Backbone, Marion
       this.options = options || {};
       //Define the template to use for this view by using tpl to fetch it and pass it to handlbars
       this.template = Handlebars.compile(tpl.get(this.options.contentName));
+      VirusUserTab.sortBy = "totalDesc";
+      $('#totalViruses').html('&#9660 Total Viruses and PUPs');
+    },
+
+    //will be called when the view is shown
+    onShow : function()
+    {
+      //add an arrow to the total header when the view loads
+      $('#totalViruses').html('&#9660 Total Viruses and PUPs');
     },
 
     //Define an id field for this view. The tab for this view is a div by default
@@ -22,13 +31,120 @@ SysAdminApp.module('VirusUserTab', function (VirusUserTab, App, Backbone, Marion
 
     //Define events for the view
     events: {
-      'click #addNew' : 'addNew'
+      'click #addNew' : 'addNew',
+      'click #userName' : 'sortByName',
+      'click #scanDate' : 'sortByDate',
+      'click #totalViruses' : 'sortByVirus',
     },
 
     //When the add New button is clicked, this function will run
     addNew: function() {
       //Call the showUserAddModal function in the Virus User controller
       VirusUserTab.VirusUserController.showUserAddModal();
+    },
+
+    //function to sort the user's list by a different method
+    sortByName : function() {
+      //if the list is already sorted by that method, it swaps it to the reverse
+      if (VirusUserTab.sort == "nameAsc")
+      {
+        //removes the arrows from the headers
+        this.clearArrows();
+        //declares the new global sortBy
+        VirusUserTab.sort = "nameDesc";
+        //gets a new user list that is pre-sorted
+        VirusUserTab.usersList.fetch({data: {sort : VirusUserTab.sort}});
+        //must reset inorder to see the changes
+        VirusUserTab.usersList.reset();
+        //adds an arrow to the header
+        $('#userName').html("&#9650 User's Name");
+      }
+      //if it is sorted by any other method
+      else
+      {
+        //removes the arrows from the headers
+        this.clearArrows();
+        //sets the global sortBy
+        VirusUserTab.sort = "nameAsc";
+        //gets a new user list that is pre-sorted
+        VirusUserTab.usersList.fetch({data: {sort : VirusUserTab.sort}});
+        //must reset in order to see the changes
+        VirusUserTab.usersList.reset();
+        //adds an arrow to the header
+        $('#userName').html("&#9660 User's Name");
+      }
+    },
+
+    //function to sort the user's list by a different method
+    sortByDate : function() {
+      //if the list is already sorted by that method, it swaps it to the reverse
+      if (VirusUserTab.sort == "dateAsc")
+      {
+        //removes the arrows from the headers
+        this.clearArrows();
+        //declares the new global sortBy
+        VirusUserTab.sort = "dateDesc";
+        //gets a new user list that is pre-sorted
+        VirusUserTab.usersList.fetch({data: {sort : VirusUserTab.sort}});
+        //must reset inorder to see the changes
+        VirusUserTab.usersList.reset();
+        //adds an arrow to the header
+        $('#scanDate').html('&#9650 Most Recent Scan Date');
+      }
+      //if it is sorted by any other method
+      else
+      {
+        //removes the arrows from the headers
+        this.clearArrows();
+        //sets the global sortBy
+        VirusUserTab.sort = "dateAsc";
+        //gets a new user list that is pre-sorted
+        VirusUserTab.usersList.fetch({data: {sort : VirusUserTab.sort}});
+        //must reset in order to see the changes
+        VirusUserTab.usersList.reset();
+        //adds an arrow to the header
+        $('#scanDate').html('&#9660 Most Recent Scan Date');
+      }
+    },
+
+    //function to sort the user's list by a different method
+    sortByVirus : function() {
+      //if the list is already sorted by that method, it swaps it to the reverse
+      if (VirusUserTab.sort == "totalDesc")
+      {
+        //removes the arrows from the headers
+        this.clearArrows();
+        //declares the new global sortBy
+        VirusUserTab.sort = "totalAsc";
+        //gets a new user list that is pre-sorted
+        VirusUserTab.usersList.fetch({data: {sort : VirusUserTab.sort}});
+        //must reset inorder to see the changes
+        VirusUserTab.usersList.reset();
+        //adds an arrow to the header
+        $('#totalViruses').html('&#9660 Total Viruses and PUPs');
+      }
+      //if it is sorted by any other method
+      else
+      {
+        //removes the arrows from the headers
+        this.clearArrows();
+        //sets the global sortBy
+        VirusUserTab.sort = "totalDesc
+        //gets a new user list that is pre-sorted";
+        VirusUserTab.usersList.fetch({data: {sort : VirusUserTab.sort}});
+        //must reset in order to see the changes
+        VirusUserTab.usersList.reset();
+        //adds an arrow to the header
+        $('#totalViruses').html('&#9650 Total Viruses and PUPs');
+      }
+    },
+
+    //removes the arrows from the headers. This is called before resorting the users
+    clearArrows : function()
+    {
+      $('#userName').html("User's Name");
+      $('#scanDate').html('Most Recent Scan Date');
+      $('#totalViruses').html('Total Viruses and PUPs');
     }
     
   });
